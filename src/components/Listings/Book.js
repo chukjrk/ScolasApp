e669f97d18cef4e1561c204c33e38b78323311ab
rushwender <rushwender@gmail.com>
2017-12-15 05:23:20 +0800

    remove foreground task

--- src/components/Listings/Book.js
+++ src/components/Listings/Book.js
@@ -202,7 +202,7 @@ export default class Book extends Component {
 
       //run the nofitication. This.constructor.function works if function called has static with it.
       //commented. uncomment if want notification pop up in foreground after user click buy item.
-          this.constructor.runSendNotification(this.props.appStore.user.uid);
+          // this.constructor.runSendNotification(this.props.appStore.user.uid);
 
       // Start and schedule BackgroundTask. See react-native-background-task docs for how to set specific schedule
       // like run after 30 minutes or etcetra
