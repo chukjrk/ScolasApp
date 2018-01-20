3beeb20d97aee959feb0f120e359008b7c4256c8
chuk <chuk@localhost.localdomain>
2018-01-19 20:06:51 -0600

    trump be everywhere

--- ios/BooXchange/AppDelegate.m
+++ ios/BooXchange/AppDelegate.m
@@ -11,6 +11,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <React/RCTPushNotificationManager.h>
 
 @implementation AppDelegate
 @synthesize oneSignal = _oneSignal;
